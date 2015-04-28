2788243f66f51ba065d71ce7ef052261f65a9727
Winhosa <k.wang0905@hotmail.com>
2015-04-28 09:49:01 +0200

    save

--- Webpage Effects/MessageBoard1/js/control.js
+++ Webpage Effects/MessageBoard1/js/control.js
@@ -1,10 +1,43 @@
-
-var get = {
+/* get DOM elements by id, class or tagname*/
+var getMan = {
     byId: function(id) {
         return typeof id === 'string' ? document.getElementById(id) : id;
     },
     
     byClass: function(className) {
-        
+        return document.getElementsByClassName(className);
+    },
+    
+    byTagName: function(tagName) {
+        return typeof tagName === 'string' ? document.getElementsByTagName(tagName) : tagName;
     }
 };
+
+/* manipulate css */
+var cssMan = {
+    
+};
+
+/* attach/remove event to objects */
+var eventMan = {
+    addEvent: function(obj, event, fcn) {
+        obj.addEventListener(event, fcn, false);
+    },
+    
+    removeEvent: function(obj, event, fcn) {
+        obj.removeEventListener(event, fcn, false);
+    }
+}
+
+/* main */
+// user's msg
+var msgItems = getMan.byClass('msg-item');
+
+for (var i = 0, len = msgItems.length; i < len; i++) {
+    eventMan.addEvent(msgItems[i], 'mouseover', function(liObj) {
+        console.log(liObj);
+        liObj.style.backgroundColor = '#dee4ea';
+    }(msgItems[i]));
+}
+
+function sendFcn() {};
