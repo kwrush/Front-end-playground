15c0ef3861fc6462fd45c04ed18f9930a1a6b709
Kai Wang <k.wang0905@hotmail.com>
2015-08-01 22:08:48 +0200

    save

--- practice/task02/js/task02_5.js
+++ practice/task02/js/task02_5.js
@@ -7,6 +7,40 @@
     
 }());
 
-var dragDrop = (function() {
+var dragDrop = (function(util) {
     
-}());
+    return {
+        /**
+         make the given DOM elements draggable, so assign listener to mousedown event
+         @param {object} item block DOM element that can be dragged
+        */
+        assignDrag: function(items) {
+            
+            this.items = items
+            
+            for(var i = 0, len = this.items.length; i < len; i++) {
+                util.addEvent(this.item[i], 'mousedown', this.handleMouseDown);
+            }
+        },
+        
+        /**
+         Listener to mousedown event on the draggable element
+         */
+        handleMouseDown: function(evt) {
+            var target = this;
+            
+        }, 
+        
+        startDrag: function(target) {
+            
+        },
+        
+        releaseDrag: function() {
+        
+        },
+        
+        
+        
+    };
+    
+}(_util));
