1cced9c6b00da6e6c76dc5f02eec1bc760d9833a
Winhosa <k.wang0905@hotmail.com>
2015-09-17 12:18:44 +0200

    minor changes

--- practice/task03/js/helper.js
+++ practice/task03/js/helper.js
@@ -1,3 +1,47 @@
-(function() {
+(function(window) {
+    'use strict';
     
-}());
+    // short terms of some common functions
+    window.qs = function (selector, scope) {
+        return (scope || document).querySelector(selector);
+    };
+    
+    window.qsa = function (selector, scope) {
+        return (scope || document).querySelectorAll(selector);
+    };
+    
+    window.gid = function (id, scope) {
+        return (scope || document).getElementById(id);
+    };
+    
+    window.gc = function (className, scope) {
+        return (scope || document).getElementsByClassName(className);
+    };
+    
+    window.gt = function (tag, scope) {
+        return (scope || document).getElementsByTagName(tag);
+    };
+    
+    // utility functions
+    window.hasClass = function (element, className) {
+        var classNames = element.className;
+        
+        if (!classNames) return false;
+        
+        classNames = classNames.split(/\s+/);
+        
+        for (var i = 0, len = classNames; i < len; i++) {
+            if (className === classNames[i]) return true;
+        }
+        
+        return false;
+    };
+    
+    window.addClass = function (element, className) {
+        if (!hasClass(element, className)) {
+            element.className += className;
+        }
+        
+        return element;
+    }
+}(window));
