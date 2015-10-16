aabf8e20cab9847e751a184ac7104ab5f8f6e190
Kai Wang <k.wang0905@hotmail.com>
2015-10-16 22:08:58 +0200

    add something

--- practice/others/bootsrap and jQuery/js/jq-test1.js
+++ practice/others/bootsrap and jQuery/js/jq-test1.js
@@ -8,4 +8,15 @@ $(document).ready(function() {
 	});
 
 	$('a.test-link').addClass('alt-a');
+
+
+	var t = $('#target'),
+		ul = $('ul.menu');
+
+	t.click(function() {
+		/*ul.toggle('slow', function() {
+			console.log('toggle done!!!');
+		});*/
+		ul.slideToggle('easeOutCubic');
+	});
 });
