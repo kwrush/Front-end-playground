46f75683325448c5e9716c8db0a7b098bd32b82b
Kai Wang <k.wang0905@hotmail.com>
2015-10-24 01:17:46 +0200

    finish feature section

--- practice/Demo/ActiveBox/js/main.js
+++ practice/Demo/ActiveBox/js/main.js
@@ -4,6 +4,28 @@ $(document).ready(function() {
     var $navToggle = $('button.nav-toggle');
     var $nav = $('.navbar');
     var header = document.getElementById('header');
+
+    /* Scroll to the selected anchor in navigation */
+    (function() {
+    	$('.navmenu a[href^="#"]').on('click', function(event) {
+
+    		var target = $(this.hash);
+
+    		if (target.length) {
+
+    			event.preventDefault();
+
+    			var $html = $('html, body');
+
+    			// 75 is the general difference before and after we modify 
+    			// the padding of the header
+    			$html.animate({
+    				scrollTop: target.offset().top - 75
+    			}, 600);
+    		}
+    	});
+    } ());
+
     
     /* Expand header when scroll to the top of page */
 	(function() {
@@ -13,7 +35,7 @@ $(document).ready(function() {
 		$win.scroll(function() {
 			var pos = $win.scrollTop();
 
-			if (pos <= headerHeight / 3) {
+			if (pos <= 35) {
 				$h.removeClass('shrink');
 			}
 			else {
