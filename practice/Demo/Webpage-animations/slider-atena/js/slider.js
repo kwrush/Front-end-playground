b07eb1ee4dcffd4ebab4e6fa71fcee8711a230ae
Kai Wang <k.wang0905@hotmail.com>
2015-11-11 23:34:03 +0100

    save

--- practice/Demo/Webpage-animations/slider-atena/js/slider.js
+++ practice/Demo/Webpage-animations/slider-atena/js/slider.js
@@ -2,14 +2,17 @@ var Slider = (function($) {
 	'use strict';
 
 	/*
-     * Make slider functioning
-     * @param {plain object} option  
-     * return slider object
+	 * Make slider functioning
+	 * @param {plain object} option  
+	 * return slider object
 	 */
 	var slider = function(option) {
-		this.index = 0;
-		this.interval = 2000;
-		this.loop = 1;
+		this.index      = 0;
+		this.interval   = 2000;
+		this.loop       = 1;
+		this.elemInFrom = '230px';
+		this.elemOutTo  = '-150px';
+
 
 		$.extend(this, option);
 
@@ -21,7 +24,7 @@ var Slider = (function($) {
 		this.$pre       = $('.pre-arrow');
 		this.$next      = $('.next-arrow');
 		this.$btn       = $('.btn-container');
-		this.$text      = $('.text-container');
+		this.$title     = $('.text-container');
 
 		this.count = this.$layer.length;
 
@@ -53,8 +56,109 @@ var Slider = (function($) {
 
 	// Show current visible slide
 	slider.prototype.showCurrent = function() {
-		this.$layer.addClass('hide');
-		$(this.$layer[this.index]).removeClass('hide');
+
+		var self = this;
+
+		self.hideTitle();
+		self.hideButton();
+
+
+		self.$layer.not($(self.$layer[self.index]).removeClass('hide'))
+		           .each(function() {
+		           		$(this).addClass('hide');
+		           });
+		//$(self.$layer[self.index]).removeClass('hide');
+
+		clearTimeout(this.timer);
+		this.timer = null;
+
+		this.timer = setTimeout(function() {
+			self.showTitle()
+				.showButton();
+		}, 2000);
+
+		return self;
+	};
+
+	slider.prototype.delay = function(delay) {
+
+		$(this).delay(delay);
+
+		return this;
+	};
+
+	slider.prototype.showButton = function() {
+
+		var btn = this.$btn.removeClass('show-btn')[this.index];
+
+		// move button group to the inital position
+		$(btn).css({"margin-left": this.elemInFrom});
+		
+		$(btn).addClass('show')
+			  .stop()
+			  .animate({ "margin-left": 0 }, 1300, 'easeOutElastic');
+
+		return this;	
+	};
+
+	slider.prototype.hideButton = function() {
+
+		var btn = this.$btn[this.lastIndex];
+
+		$(btn).stop()
+			  .animate({ "margin-left": this.elemOutTo }, 1000)
+		      .removeClass('show');
+
+		return this;
+	};
+
+	slider.prototype.showTitle = function() {
+		var $mainTitle = $('h2', this.$title[this.index]);
+		var $subTitle = $('p', this.$title[this.index]);
+
+		$mainTitle.css({'transform': 'skewX(-90deg)'});
+		$subTitle.css({'margin-left': this.elemInFrom});
+
+		$mainTitle.addClass('show')
+				  .stop()
+				  .animate(
+				  	{ 
+				  		'transform': 'skewX(0)' 
+				  	}, 
+				  	{
+				  		duration: 1300,
+				  		easing: 'easeOutElastic',
+				  		step: function(now, tween) {
+				  			$(this).css('transform', 'skewX('+ now +'deg)');
+				  		}
+				  	});
+		
+		$subTitle.addClass('show')
+			     .stop()
+			     .animate({ "margin-left": 0 }, 1300, 'easeOutElastic');
+
+		return this;
+	};
+
+	slider.prototype.hideTitle = function() {
+		var $mainTitle = $('h2', this.$title[this.lastIndex]);
+		var $subTitle = $('p', this.$title[this.lastIndex]);
+
+		$mainTitle.stop()
+				  .animate(
+				  	{ 'transform': 'skewX(-90deg)' }, 
+
+				  	{
+				  		duration: 1000,
+				  		step: function(now, tween) {
+				  			$(this).css('transform', 'skewX('+ now +'deg)' );
+				  		}
+				  	})
+				  .removeClass('show');
+
+		$subTitle.stop()
+			     .animate({ "margin-left": this.elemOutTo }, 1000)
+		         .removeClass('show');
 
 		return this;
 	};
@@ -64,7 +168,7 @@ var Slider = (function($) {
 		$(this.$spots[this.index]).addClass('active');
 
 		return this;
-	}
+	};
 
 	// Go to previous slide
 	slider.prototype.pre = function() {
@@ -84,6 +188,7 @@ var Slider = (function($) {
 
 		if (index === -1) return;
 
+		this.lastIndex = this.index;
 		this.index = index;
 
 		this.showCurrent().highlightSpot();
