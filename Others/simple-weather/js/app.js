141563da736719598954ba93bdf00901e2639799
strivk <winhosa0905@gmail.com>
2016-07-11 17:23:21 +0200

    first commit

---
+++ Others/simple-weather/js/app.js
@@ -0,0 +1,10 @@
+var weatherApp = (function () {
+    loader.load(['js/lib/underscore-min.js',
+                 'js/constants.js',
+                 'js/events.js',
+                 'js/cities.js',
+                 'js/view.js',
+                 'js/controller.js'], function () {
+
+    })
+})();
---
+++ Others/simple-weather/js/constants.js
@@ -0,0 +1,5 @@
+var constants = (function () {
+    return {
+        AUTO_COMPLETE_URL: 'http://autocomplete.wunderground.com/aq?query=',
+    };
+})();
