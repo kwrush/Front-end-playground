0433eeaac57e3522cff48d75d73c86eef68c1a9d
Kai Wang <k.wang0905@hotmail.com>
2016-07-12 22:46:54 +0200

    save

---
+++ Others/simple-weather/app/js/cities.js
@@ -0,0 +1,104 @@
+/**
+ * City class
+ */
+function City (query, data) {
+    this.query = query || '';
+    this.data = data || null;
+}
+
+City.prototype = function () {
+    function _valid (data) {
+        return data.response && data.location && data.current_observation;
+    }
+
+    function _url () {
+        return constants.url(this.query);
+    }
+
+    function _get () {
+        var self = this;
+        return $.ajax({
+            url: this.url(),
+            method: 'GET',
+            dataType: 'jsonp',
+        });
+    }
+
+    return {
+        url: _url,
+        get: _get,
+        valid: _valid
+    };
+}();
+
+/**
+ * Collection of city
+ */
+function Cities (dbName) {
+    this.dbName = dbName || 'simple-weather';
+    this.save();
+}
+
+Cities.prototype = function () {
+    var _cities = [];
+
+    function _getCities () {
+        return _cities;
+    }
+
+    function _isValidCity (city) {
+        return city.data.response &&
+               city.data.location &&
+               city.data.current_observation &&
+               !_isDuplicate(city);
+    }
+
+    function _isDuplicate (city) {
+        _cities.forEach(function (aCity) {
+            if (aCity.query === city.query) return true;
+        });
+        return false;
+    }
+
+    function _addCity (city) {
+        if (_isValidCity(city)) {
+            _cities.push(city);
+            this.save();
+            $(document).trigger('cityAdded', [_cities, city]);
+        }
+    }
+
+    function _getCity(index) {
+        return _cities[index];
+    }
+
+    function _removeCity(index) {
+        var city =  _cities.splice(index, 1);
+        if (city.length) {
+            this.save();
+            $(document).trigger('cityRemoved', [_cities, city]);
+        }
+    }
+
+    // local storage
+    function _save () {
+        localStorage.setItem(this.dbName, JSON.stringify(_cities));
+    }
+
+    function _load () {
+        var raw = localStorage.getItem(this.dbName);
+        if (raw) {
+            var data = JSON.parse(raw);
+            if (data) _cities = data;
+        }
+    }
+
+    return {
+        getCities: _getCities,
+        addCity: _addCity,
+        getCity: _getCity,
+        removeCity: _removeCity,
+        load: _load,
+        save: _save
+    };
+}();
---
+++ Others/simple-weather/app/js/controller.js
@@ -0,0 +1,64 @@
+function Controller (cities, view) {
+    this.cities = new Cities();
+    this.view = new View();
+    this.init();
+}
+
+Controller.prototype = function () {
+    function _init () {
+        $(document).on('newCity', _.bind(this.newCity, this));
+        $(document).on('getSucceed', _.bind(this.upateCityView, this));
+        $(document).on('cityAdded', _.bind(this.updateCitiesView, this));
+        $(document).on('cityRemoved', _.bind(this.updateCitiesView, this));
+    }
+
+    // Update individual city's view
+    function _updateCityView (event, eventData) {
+
+    }
+
+    function _updateView (event, eventData) {
+
+    }
+
+    // append or remove city view block
+    function _updateCitiesView (event, eventData) {
+        event.preventDefault();
+        var self = this;
+        if (event.type === 'cityAdded') {
+
+
+        } else if (event.type === 'cityRemoved') {
+
+        }
+    }
+
+    // fires when we select a city from result list
+    // TODO: use promise, first show spin loader, if get() success, add city view, 
+    // otherwise show alert and remove empty city view, finally remove loader
+    function _newCity (event, eventData) {
+        event.preventDefault();
+
+        var self = this;
+        if (event.type === 'newCity') {
+            var city = new City(eventData);
+            city.get()
+                .success(function (response) {
+                    if (city.valid(response)) {
+                        city.data = response;
+                        self.cities.addCity(city);
+                    }
+                }).fail(function (error) {
+                    self.view.alert('Cannot get weather data. Error:' + error);
+                });
+        }
+    }
+
+    return {
+        init: _init,
+        newCity: _newCity,
+        updateView: _updateView,
+        updateCityView: _updateCityView,
+        updateCitiesView: _updateCitiesView
+    };
+}();
