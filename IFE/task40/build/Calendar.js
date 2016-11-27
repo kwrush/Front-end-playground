55e73225844e23828ce3956617d0296ba15dbda8
Kai Wang <k.wang0905@hotmail.com>
2016-11-27 21:43:39 +0100

    save

--- IFE/task40/build/Calendar.js
+++ IFE/task40/build/Calendar.js
@@ -5,27 +5,15 @@ var Calendar = function (options) {
     var MONTH = 'January, February, March, April, May, June, July, August, September, October, November, December'.split(/,\s*/);
     var WEEKDAY = 'Mon, Tue, Wed, Thu, Fri, Sat, Sun'.split(/,\s*/);
 
-    // All years can be reached
-    var AVAILABLE_YEARS = [1916, 2116];
-
-    // default container
-    var CONTAINER = document.querySelector('.calendar') || document.body;
-    var DAY_ROW_NUMS = 6;
-    var DAY_COL_NUMS = 7;
-
     // calendar main template
     var TEMPLATE =
-    '<div class="calendar-header">'                                              +
-    '    <h2 class="calendar-title"></h2>'                                       +
-//    '    <div class="calendar-nav calendar-nav-prev">'                           +
-    '        <span class="calendar-nav-button calendar-nav-prev-button"></span>' +
-//    '    </div>'                                                                 +
-//    '    <div class="calendar-nav calendar-nav-next">'                           +
-    '        <span class="calendar-nav-button calendar-nav-next-button"></span>' +
-//    '    </div>'                                                                 +
+    '<div class="calendar-header">'                                          +
+    '    <h2 class="calendar-title"></h2>'                                   +
+    '    <span class="calendar-nav-button calendar-nav-prev-button"></span>' +
+    '    <span class="calendar-nav-button calendar-nav-next-button"></span>' +
     '</div>'                                                    +
     '<div class="calendar-pane">'                               +
-    '    <div class="calendar-view calendar-weekday-view">'     +
+    '    <div class="calendar-view calendar-date-view">'     +
     '        <ul class="calendar-weekday-header"></ul>'         +
     '        <div class="calendar-view-wrap">'                  +
     '            <div class="calendar-prev-view"></div>'        +
@@ -49,28 +37,13 @@ var Calendar = function (options) {
     // '    </div>'                                                +
     '</div>';
 
-    var PREV_ICON =
-    '<svg class="calendar-nav-icon" viewBox="0 0 24 24">'                 +
-    '    <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path>' +
-    '</svg>';
-
-    var NEXT_ICON =
-    '<svg class="calendar-nav-icon" viewBox="0 0 24 24">'                 +
-    '    <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path>' +
-    '</svg>';
-
-
     var defaults = {
-        prevIcon: PREV_ICON,
-        nextIcon: NEXT_ICON,
-        weekDayFormat: WEEKDAY,
-        monthFormat: MONTH,
-        availableYears: AVAILABLE_YEARS,
-        dayRows: DAY_ROW_NUMS,
-        dayCols: DAY_COL_NUMS
+        width: 330,
+        height: 280,
+        callback: {
+            select: function () {}
+        }
     };
-
-    //defaults.weekDayFormat = WEEKDAY.
 
     options = options || {};
 
@@ -80,9 +53,12 @@ var Calendar = function (options) {
         }
     }
 
+    defaults.month = MONTH;
+    defaults.weekday = WEEKDAY;
+
     this.options = options;
-    this.container = CONTAINER;
-    this.mainTemplate = TEMPLATE;
+    this._defaults = defaults;
+    this._mainTemplate = TEMPLATE;
     this.init();
 };
 
@@ -93,27 +69,36 @@ Calendar.prototype = {
      * Entry point of initiation
      */
     init: function () {
-        this.date = new Date();
-        this.render();
+        this.container = this._initContainer();
 
         var con = this.container;
+        con.style.width = this.options.width + 'px';
+        con.style.height = this.options.height + 'px';
+
+        con.innerHTML = this._mainTemplate;
+
         this.calendarHead = con.querySelector('.calendar-header');
         this.calendarPane = con.querySelector('.calendar-pane');
 
-        this.calendarTitle = this.calendarHead.querySelector('.calendar-title');
-        this.prevNavButton = this.calendarHead.querySelector('.calendar-nav-prev-button');
-        this.nextNavButton = this.calendarHead.querySelector('.calendar-nav-next-button');
-        this.weekdayView = this.calendarPane.querySelector('.calendar-weekday-view');
+        this.calendarPane.style.width = '100%';
+        this.calendarPane.style.height = con.clientHeight - this.calendarHead.offsetHeight + 'px';
 
-        this._initHeaderView();
-        this._initWeekdayView();
+        this.calendarTitle = con.querySelector('.calendar-title');
+        this.prevNavButton = con.querySelector('.calendar-nav-prev-button');
+        this.nextNavButton = con.querySelector('.calendar-nav-next-button');
+        this.dateView      = con.querySelector('.calendar-date-view');
+
+        this._initCalendarHeader();
+        this._initDateView();
+
+        this._listen();
     },
 
     /*
      * Render view
      */
     render: function () {
-        this.container.innerHTML = this.mainTemplate;
+        this.container.innerHTML = this._mainTemplate;
     },
 
     /*
@@ -130,66 +115,81 @@ Calendar.prototype = {
 
     },
 
-    _initView: function () {
-
+    _initContainer: function () {
+        var con = document.getElementsByClassName('calendar')[0];
+        if (con) {
+            return con;
+        } else {
+            con = document.createElement('div');
+            con.className = 'calendar';
+            document.body.appendChild(con);
+            return con;
+        }
     },
 
-    _initHeaderView: function () {
+    _initCalendarHeader: function () {
         var date = new Date();
         this.calendarTitle.innerText = this._getMonthText(date.getMonth()) + ' ' + date.getFullYear();
-        this.prevNavButton.innerHTML = this.options.prevIcon;
-        this.nextNavButton.innerHTML = this.options.nextIcon;
+        this.prevNavButton.innerHTML = this._getPrevIcon();
+        this.nextNavButton.innerHTML = this._getNextIcon();
     },
 
-    _initWeekdayView: function () {
+    _initDateView: function () {
         var date = new Date(),
             year = date.getFullYear(),
             month = date.getMonth();
 
-        var header   = this.weekdayView.querySelector('.calendar-weekday-header');
-        var currView = this.weekdayView.querySelector('.calendar-curr-view');
-        var prevView = this.weekdayView.querySelector('.calendar-prev-view');
-        var nextView = this.weekdayView.querySelector('.calendar-next-view');
+        this.dateWrap = this.dateView.querySelector('.calendar-view-wrap');
+        this.weekdayHead = this.dateView.querySelector('.calendar-weekday-header');
 
-        header.innerHTML   = header && this._weekdayViewHeader();
-        currView.innerHTML = currView && this._weekdayViewOfMonth(year, month);
-        //prevView.innerHTML = prevView && this._weekdayViewOfMonth(year, month - 1);
-        //nextView.innerHTML = nextView && this._weekdayViewOfMonth(year, month + 1);
+        this.dateWrap.style.width = '100%';
+        this.dateWrap.style.height = this.dateView.clientHeight - this.weekdayHead.clientHeight + 'px';
+
+        this.currDateView = this.dateWrap.querySelector('.calendar-curr-view');
+        this.prevDateView = this.dateWrap.querySelector('.calendar-prev-view');
+        this.nextDateView = this.dateWrap.querySelector('.calendar-next-view');
+
+        this.weekdayHead.innerHTML = this._getWeekdayHeader();
+        this.currDateView.innerHTML = this._getDateViewsOfMonth(year, month);
+        this.prevDateView.innerHTML = this._getDateViewsOfMonth(year, month - 1);
+        this.nextDateView.innerHTML = this._getDateViewsOfMonth(year, month + 1);
     },
 
     _initMonthView: function () {
 
     },
 
-    _initYearView: function () {
-
-    },
-
-    _weekdayViewHeader: function () {
-        var lis = '';
-        for (var i = 0, len = this.options.weekDayFormat.length; i < len; i++) {
+    _getWeekdayHeader: function () {
+        var lis = [],
+            width = 100 / this._defaults.weekday.length;
+            height = 32;
+        for (var i = 0, len = this._defaults.weekday.length; i < len; i++) {
             var li = document.createElement('li');
-            li.innerText = this.options.weekDayFormat[i];
-            lis += li.outerHTML;
+            li.innerText = this._defaults.weekday[i];
+            li.style.width = width + '%';
+            li.style.height = height + 'px';
+            li.style.lineHeight = height + 'px';
+            lis.push(li.outerHTML);
         }
 
-        return lis;
+        return lis.join('');
     },
 
-    _weekdayViewOfMonth: function (year, month) {
+    _getDateViewsOfMonth: function (year, month) {
         var date  = new Date(year, month),
-            rows  = this.options.dayRows,
-            cols  = this.options.dayCols;
+            rows  = 6,
+            cols  = this._defaults.weekday.length;
 
         var dayIndex = 0,
-            dayView = '',
+            dayView = [],
             today = new Date().setHours(0, 0, 0, 0),
             firstDayOfMonth = this._firstDayOfMonth(year, month),
             daysOfThisMonth = this._daysInCurrMonth(year, month),
             daysOfPrevMonth = this._daysInPrevMonth(year, month),
             daysOfNextMonth = this._daysInNextMonth(year, month);
 
-        var dayTileWidth = 1 / cols * 100;
+        var dayTileWidth = this.dateWrap.clientWidth / cols,
+            dayTileHeight = this.dateWrap.clientHeight / rows;
 
         for (var i = 0; i < rows; i++) {
             var row = i;
@@ -211,40 +211,71 @@ Calendar.prototype = {
                         classNames.push('calendar-next-month-day');
                     } else {
                         dayTime = new Date(year, month, dayNumber).getTime();
-                        if (dayTime === today) classNames.push('calendar-today');
+                        if (dayTime === today) classNames.push('calendar-today selected');
                     }
                 }
 
                 var d = new Date(dayTime);
-                var span = document.createElement('span');
-                span.className = classNames.join(' ');
-                span.style.width = dayTileWidth + '%';
-                span.innerText = d.getDate();
-                span.setAttribute('data-year', d.getFullYear());
-                span.setAttribute('data-month', d.getMonth());
-                span.setAttribute('data-date', d.getDate());
-                dayView += span.outerHTML;
+                var dateView = this._getDateView(d, classNames, dayTileWidth, dayTileHeight);
+                dayView.push(dateView);
             }
         }
 
-        return dayView;
+        return dayView.join('');
     },
 
     _monthView: function (date) {
 
     },
 
-    _yearView: function (date) {
+    _getDateView: function (date, classNames, width, height) {
+        var div = document.createElement('div');
+        var span = document.createElement('span');
 
+        div.appendChild(span);
+
+        div.className = classNames.join(' ');
+        div.style.width = width + 'px';
+        div.style.height = height + 'px';
+        div.style.lineHeight = height + 'px';
+        div.setAttribute('data-year', date.getFullYear());
+        div.setAttribute('data-month', date.getMonth());
+        div.setAttribute('data-date', date.getDate());
+
+        var size;
+        if (width > height) {
+            size = (height > 34 ? 34 : height) + 'px';
+        } else {
+            size = (width > 34 ? 34 : width) + 'px';
+        }
+        span.style.display = 'block';
+        span.style.width = size;
+        span.style.height = size;
+        span.style.lineHeight = size;
+        span.innerText = date.getDate();
+
+        return div.outerHTML;
     },
 
     _getMonthText: function (month) {
         var m = month || new Date().getMonth();
-        return this.options.monthFormat[m];
+        return this._defaults.month[m];
     },
 
     _firstDayOfMonth: function (year, month) {
         return new Date(year, month).getDay();
+    },
+
+    _getPrevIcon: function () {
+        return '<svg class="calendar-nav-icon" viewBox="0 0 24 24">'                 +
+               '    <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path>' +
+               '</svg>';
+    },
+
+    _getNextIcon: function () {
+        return '<svg class="calendar-nav-icon" viewBox="0 0 24 24">'                 +
+               '    <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path>' +
+               '</svg>';
     },
 
     /**
@@ -275,6 +306,31 @@ Calendar.prototype = {
         var y = year || new Date().getFullYear();
         var m = month || new Date().getMonth() + 1;
         return new Date(y, m, 0).getDate();
+    },
+
+    _listen: function () {
+        this.calendarHead.addEventListener('click', this._handleNavigation.bind(this));
+    },
+
+    _handleNavigation: function (event) {
+        event = event || window.event;
+        event.preventDefault();
+
+        var target = event.target || event.srcElement;
+
+        while (target.tagName !== 'SPAN') {
+            target = target.parentElement;
+        }
+
+        if (target === this.nextNavButton) {
+            this.nextDateView.style.transform = 'translateX(0)';
+            this.currDateView.style.transform = 'translateX(-100%)';
+            this.prevDateView.style.transform = 'translateX(-200%)';
+        } else if (target === this.prevNavButton) {
+            this.nextDateView.style.transform = 'translateX(200%)';
+            this.currDateView.style.transform = 'translateX(100%)';
+            this.prevDateView.style.transform = 'translateX(0)';
+        }
     },
 
     _isDate: function (date) {
